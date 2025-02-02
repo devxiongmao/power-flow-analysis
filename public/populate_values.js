32c99152ae509a1c94c82c3ba0faf0cfc799e5c9
Jon <39038058+devxiongmao@users.noreply.github.com>
2025-02-01 21:59:54 -0500

    XMDEV-115: Updates JS functions to be more readable (#2)

--- public/populate_values.js
+++ public/populate_values.js
@@ -1,24 +1,11 @@
-function populateValues(sourceValue, idValue){
-  var num = sourceValue.match(/\d+/);
-  var x = document.getElementById(sourceValue).value;
+function populateValues(sourceValue, idValue) {
+  const numMatch = sourceValue.match(/\d+/);
+  if (!numMatch) return;
 
-  num = parseInt(num, 10);
+  const num = parseInt(numMatch[0], 10);
+  const x = document.getElementById(sourceValue)?.value;
+  if (x === undefined) return;
 
-  if (num % 8 == 0){
-    document.getElementById(idValue).innerHTML = "QMax:  " + x;
-  }else if(num % 8 == 1){
-    document.getElementById(idValue).innerHTML = "V:  " + x;
-  }else if(num % 8 == 2){
-    document.getElementById(idValue).innerHTML = "A:  " + x;
-  }else if(num % 8 == 3){
-    document.getElementById(idValue).innerHTML = "PGi:  " + x;
-  }else if(num % 8 == 4){
-    document.getElementById(idValue).innerHTML = "QGi:  " + x;
-  }else if(num % 8 == 5){
-    document.getElementById(idValue).innerHTML = "PLi:  " + x;
-  }else if(num % 8 == 6){
-    document.getElementById(idValue).innerHTML = "QLi:  " + x;
-  }else if(num % 8 == 7){
-    document.getElementById(idValue).innerHTML = "QMin:  " + x;
-  }
+  const labels = ["QMax", "V", "A", "PGi", "QGi", "PLi", "QLi", "QMin"];
+  document.getElementById(idValue).innerHTML = `${labels[num % 8]}:  ${x}`;
 }
