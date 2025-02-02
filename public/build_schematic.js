9423acc5d080e8b57cef1fdae14460d1de4d252b
Jon <39038058+devxiongmao@users.noreply.github.com>
2025-02-01 21:59:54 -0500

    XMDEV-115: Updates JS functions to be more readable (#2)

--- public/build_schematic.js
+++ public/build_schematic.js
@@ -1,16 +1,9 @@
-function buildSchematic(){
-	for(;;){
-		var busTypes = document.getElementsByClassName("bus-select");
-		var busVoltages = document.getElementsByClassName("bus-voltage");
-		var busAngles = document.getElementsByClassName("bus-angle");
-		var busPValues = document.getElementsByClassName("bus-p");
-		var busQValues = document.getElementsByClassName("bus-q");
-
-		var tbl = document.getElementById('schematic'), // table reference
-        i;
-	    // open loop for each row and append cell
-	    for (i = 0; i < tbl.rows.length; i++) {
-	        createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length), i, 'col');
-	    }
-	}
-}
+function buildSchematic() {
+	const tbl = document.getElementById("schematic");
+	if (!tbl) return; 
+  
+	Array.from(tbl.rows).forEach((row, index) => {
+	  createCell(row.insertCell(row.cells.length), index, "col");
+	});
+  }
+  
